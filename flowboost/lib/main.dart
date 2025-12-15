f4c103511fe6630b672e65d26de264dd988ce318
Firdig Haqqy <99621102+Firdig@users.noreply.github.com>
2025-12-15 15:30:22 +0700

    Update Firebase initialization options

--- flowboost/lib/main.dart
+++ flowboost/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    // kadang pakai DefaultFirebaseOptions.currentPlatform
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
